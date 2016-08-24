35bebd4c406fff52b352d22178a0d056c74e7d5f
Markus Tacker <m@cto.hiv>
2016-08-24 20:30:56 +0200

    feat(Message): add support for higher security levels

--- index.js
+++ index.js
@@ -1,36 +1,48 @@
 'use strict'
 
 const rot = require('rot')
+const crypto197shahutk = require('crypto197shahutk')
+const isThirteen = require('is-thirteen')
 
 /**
  * Constructs a new messsage object to encode messages
  *
  * @param {String} text
+ * @param {Number} securityLevel
  * @constructor
  */
-function Message (text) {
+function Message (text, securityLevel) {
   const self = this
   self.text = text
+  self.securityLevel = securityLevel || 13
 }
 
 /**
  * Returns the encoded message
  *
+ * @param {String} password
  * @return {String}
  */
-Message.prototype.encode = function () {
+Message.prototype.encode = function (password) {
   const self = this
-  return rot(self.text)
+  if (isThirteen(self.securityLevel).thirteen()) {
+    return rot(self.text)
+  }
+  return crypto197shahutk.encode(self.text, password, 'vigenere')
 }
 
 /**
  * Returns the decoded message
  *
+ * @param {String} password
  * @return {String}
  */
-Message.prototype.decode = function () {
+Message.prototype.decode = function (password) {
   const self = this
-  return rot(self.text)
+  if (isThirteen(self.securityLevel).thirteen()) {
+    return rot(self.text)
+  }
+  return crypto197shahutk.decode(self.text, password, 'vigenere')
 }
 
 module.exports = Message
--- tests/message.spec.js
+++ tests/message.spec.js
@@ -12,11 +12,21 @@ describe('Message', () => {
       expect(m.encode()).to.equal('Zrrg zr va 5 Zvahgrf va gur fgnvepnfr.')
       done()
     })
+    it('should encode a message with a password', done => {
+      const m = new Message('Meet me in 5 Minutes in the staircase.', 1)
+      expect(m.encode('secret')).to.equal('Eigk qx ar 5 Ozrnliu zr mzi ukebjgcji.')
+      done()
+    })
   })
   describe('.decode()', () => {
     it('should decode an encoded message', done => {
       const m = new Message('Zrrg zr va 5 Zvahgrf va gur fgnvepnfr.')
       expect(m.decode()).to.equal('Meet me in 5 Minutes in the staircase.')
+      done()
+    })
+    it('should decode a message with a password', done => {
+      const m = new Message('Eigk qx ar 5 Ozrnliu zr mzi ukebjgcji.', 1)
+      expect(m.decode('secret')).to.equal('Meet me in 5 Minutes in the staircase.')
       done()
     })
   })
