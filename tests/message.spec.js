292333b179884f7737b5512660248383a3f08390
Markus Tacker <m@coderbyheart.com>
2017-12-03 17:26:02 +0100

    refactor(project): use jest, commitlint

--- tests/message.spec.js
+++ tests/message.spec.js
@@ -1,32 +1,31 @@
 'use strict'
 
-/* global describe, it */
+/* global describe, it, expect */
 
 const Message = require('../')
-const expect = require('chai').expect
 
 describe('Message', () => {
   describe('.encode()', () => {
     it('should encode a message with a password', done => {
       const m = new Message('Meet me in 5 Minutes in the staircase.')
-      expect(m.encode('secret')).to.equal('Eigk qx ar 5 Ozrnliu zr mzi ukebjgcji.')
+      expect(m.encode('secret')).toEqual('Eigk qx ar 5 Ozrnliu zr mzi ukebjgcji.')
       done()
     })
     it('should throw an error if password is empty', done => {
       const m = new Message('Meet me in 5 Minutes in the staircase.')
-      expect(m.encode).to.throw(TypeError, /\[tcomb]/)
+      expect(m.encode).toThrow(TypeError)
       done()
     })
   })
   describe('.decode()', () => {
     it('should decode a message with a password', done => {
       const m = new Message('Eigk qx ar 5 Ozrnliu zr mzi ukebjgcji.')
-      expect(m.decode('secret')).to.equal('Meet me in 5 Minutes in the staircase.')
+      expect(m.decode('secret')).toEqual('Meet me in 5 Minutes in the staircase.')
       done()
     })
     it('should throw an error if password is empty', done => {
       const m = new Message('Eigk qx ar 5 Ozrnliu zr mzi ukebjgcji.')
-      expect(m.decode).to.throw(TypeError, /\[tcomb]/)
+      expect(m.decode).toThrow(TypeError)
       done()
     })
   })
